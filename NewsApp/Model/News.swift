efd78f7cf5c476114c0201c6ddf5ab9818ef0b44
ercelserhat <ercelserhat@gmail.com>
2023-06-29 00:46:25 +0300

    Created simple model

---
+++ NewsApp/Model/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Serhat on 29.06.2023.
+//
+
+import Foundation
+
+struct News: Decodable{
+    let title: String
+    let story: String
+}
---
+++ NewsApp/View/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Serhat on 29.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
+        return cell
+    }
+}
